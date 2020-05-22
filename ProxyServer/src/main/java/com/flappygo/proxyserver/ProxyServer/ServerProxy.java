db15832bfede1e71ad934f9bc5f57bb8684630fe
lijunlin <327603258@qq.com>
2020-05-22 17:10:45 +0800

    修复返回contentlength不正确导致的延迟播放

--- ProxyServer/src/main/java/com/flappygo/proxyserver/ProxyServer/ServerProxy.java
+++ ProxyServer/src/main/java/com/flappygo/proxyserver/ProxyServer/ServerProxy.java
@@ -6,7 +6,13 @@ import com.koushikdutta.async.http.AsyncHttpGet;
 import com.koushikdutta.async.http.server.AsyncHttpServer;
 import com.koushikdutta.async.http.server.HttpServerRequestCallback;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerProxy extends AsyncHttpServer {
+
+    //移除
+    private List<String> addedStrs = new ArrayList<>();
 
     //单例
     private static ServerProxy instance;
@@ -33,22 +39,42 @@ public class ServerProxy extends AsyncHttpServer {
 
     //添加子类
     public void addVideoChildProxy(String trueAction, HttpServerRequestCallback serverRequestCallback) {
-        instance.addAction(AsyncHttpGet.METHOD, trueAction, serverRequestCallback);
+        synchronized (addedStrs) {
+            if (!addedStrs.contains(trueAction)) {
+                addedStrs.add(trueAction);
+                instance.addAction(AsyncHttpGet.METHOD, trueAction, serverRequestCallback);
+            }
+        }
     }
 
     //移除子类
     public void removeVideoChildProxy(String trueAction) {
-        instance.removeAction(AsyncHttpGet.METHOD, trueAction);
+        synchronized (addedStrs) {
+            if (addedStrs.contains(trueAction)) {
+                addedStrs.remove(trueAction);
+                instance.removeAction(AsyncHttpGet.METHOD, trueAction);
+            }
+        }
     }
 
     //添加
     public void addVideoProxy(String actionID, HttpServerRequestCallback serverRequestCallback) {
-        instance.addAction(AsyncHttpGet.METHOD, "/" + actionID, serverRequestCallback);
+        synchronized (addedStrs) {
+            if (!addedStrs.contains(actionID)) {
+                addedStrs.add(actionID);
+                instance.addAction(AsyncHttpGet.METHOD, "/" + actionID, serverRequestCallback);
+            }
+        }
     }
 
     //移除
     public void removeVideoProxy(String actionID) {
-        instance.removeAction(AsyncHttpGet.METHOD, "/" + actionID);
+        synchronized (addedStrs) {
+            if (addedStrs.contains(actionID)) {
+                addedStrs.remove(actionID);
+                instance.removeAction(AsyncHttpGet.METHOD, "/" + actionID);
+            }
+        }
     }
 
 }
